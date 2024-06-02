970e360341fbd9285c40342529fe2b9e60a56256
teodor-sakal-franciskovic <sakal.teodor@gmail.com>
2024-06-02 21:17:37 +0200

    feat: add logging

--- src/main/java/uns/ac/rs/controller/ReservationController.java
+++ src/main/java/uns/ac/rs/controller/ReservationController.java
@@ -1,5 +1,7 @@
 package uns.ac.rs.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import uns.ac.rs.config.IntegrationConfig;
 import jakarta.annotation.security.PermitAll;
 import jakarta.annotation.security.RolesAllowed;
@@ -25,6 +27,8 @@ import java.util.List;
 @Consumes(MediaType.APPLICATION_JSON)
 public class ReservationController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ReservationController.class);
+
     @Autowired
     private MicroserviceCommunicator microserviceCommunicator;
 
@@ -45,9 +49,12 @@ public class ReservationController {
                 authorizationHeader);
         String userEmail = (String) response.getData();
         if (userEmail.equals("")) {
+            logger.warn("Unauthorized access for creating reservation");
             return Response.status(Response.Status.UNAUTHORIZED).entity(response).build();
         }
+        logger.info("Creating reservation");
         Reservation reservation = reservationService.createReservation(reservationRequestDTO, userEmail);
+        logger.info("Reservation successfully created");
 
         GeneralResponse automaticReservationAcceptanceStatusResponse = microserviceCommunicator.processResponse(
                 config.userServiceAPI() + "/user/get-automatic-reservation-acceptance-status",
@@ -58,8 +65,10 @@ public class ReservationController {
         boolean isAutomaticReservationAcceptanceStatusActive = (boolean) automaticReservationAcceptanceStatusResponse.getData();
 
         if (isAutomaticReservationAcceptanceStatusActive) {
+            logger.info("Accepting reservation - automatic reservation status is active");
             reservation = reservationService.changeReservationStatus(reservation.getId(), ReservationStatus.ACCEPTED);
         }
+
 
         return Response
                 .status(Response.Status.CREATED)
@@ -81,13 +90,17 @@ public class ReservationController {
                 authorizationHeader);
         String userEmail = (String) response.getData();
         if (userEmail.equals("")) {
+            logger.warn("Unauthorized access for getting active reservations");
             return Response.status(Response.Status.UNAUTHORIZED).entity(response).build();
         }
+
+        logger.info("Retrieving active reservations for user with email {}", userEmail);
         List<Reservation> reservations = reservationService.getActiveReservations(userEmail);
         List<ReservationResponseDTO> reservationResponseDTOS = new ArrayList<>();
         for (Reservation reservation: reservations) {
             reservationResponseDTOS.add(new ReservationResponseDTO(reservation));
         }
+        logger.info("Successfully retrieved active reservations for user with email {}", userEmail);
         return Response
                 .status(Response.Status.OK)
                 .entity(new GeneralResponse<>(reservationResponseDTOS, "Successfully retrieved active reservations"))
@@ -105,13 +118,17 @@ public class ReservationController {
                 authorizationHeader);
         String userEmail = (String) response.getData();
         if (userEmail.equals("")) {
+            logger.warn("Unauthorized access for deactivating reservation");
             return Response.status(Response.Status.UNAUTHORIZED).entity(response).build();
         }
+
+        logger.info("Setting reservations status to CANCELLED");
         Reservation reservation = reservationService.changeReservationStatus(reservationId, ReservationStatus.CANCELLED);
         GeneralResponse noCancellations = microserviceCommunicator.processResponse(
                 config.userServiceAPI() + "/user/append-cancellation",
                 "GET",
                 authorizationHeader);
+        logger.info("Successfully deactivated reservation");
         return Response
                 .status(Response.Status.OK)
                 .entity(new GeneralResponse<>(new ReservationResponseDTO(reservation), "Successfully cancelled an active reservation"))
@@ -128,10 +145,15 @@ public class ReservationController {
                 authorizationHeader);
         String userEmail = (String) response.getData();
         if (userEmail.equals("")) {
+            logger.warn("Unauthorized access for getting all requested reservations");
             return Response.status(Response.Status.UNAUTHORIZED).entity(response).build();
         }
+
+        logger.info("Retrieving requested reservations");
         List<Reservation> requestedReservations = reservationService.getRequestedReservations(userEmail);
+        logger.info("Successfully retrieved requested reservations");
         List<ReservationResponseDTO> reservationResponseDTOS = new ArrayList<>();
+        logger.info("Retrieving number of cancellations for each guest");
         for (Reservation requestedReservation: requestedReservations) {
             GeneralResponse noCancellations = microserviceCommunicator.processResponse(
                     config.userServiceAPI() + "/user/no-cancellations/" + requestedReservation.getGuestEmail(),
@@ -139,6 +161,7 @@ public class ReservationController {
                     authorizationHeader);
             reservationResponseDTOS.add(new ReservationResponseDTO(requestedReservation, (int) noCancellations.getData()));
         }
+        logger.info("Retrieved number of cancellations for each guest");
         return Response
                 .status(Response.Status.OK)
                 .entity(new GeneralResponse<>(reservationResponseDTOS, "Successfully retrieved requested reservations"))
@@ -156,10 +179,13 @@ public class ReservationController {
                 authorizationHeader);
         String userEmail = (String) response.getData();
         if (userEmail.equals("")) {
+            logger.warn("Unauthorized access for rejecting a reservation");
             return Response.status(Response.Status.UNAUTHORIZED).entity(response).build();
         }
 
+        logger.info("Rejecting a reservation with id {}", reservationId);
         Reservation reservation = reservationService.rejectReservation(reservationId);
+        logger.info("Successfully rejected a reservation with id {}", reservationId);
         return Response
                 .status(Response.Status.OK)
                 .entity(new GeneralResponse<>(new ReservationResponseDTO(reservation), "Successfully rejected reservation"))
@@ -177,10 +203,13 @@ public class ReservationController {
                 authorizationHeader);
         String userEmail = (String) response.getData();
         if (userEmail.equals("")) {
+            logger.warn("Unauthorized access for accepting a reservation");
             return Response.status(Response.Status.UNAUTHORIZED).entity(response).build();
         }
 
+        logger.info("Accepting a reservation with id {}", reservationId);
         Reservation reservation = reservationService.acceptReservation(reservationId);
+        logger.info("Successfully accepted a reservation with id {}", reservationId);
         return Response
                 .status(Response.Status.OK)
                 .entity(new GeneralResponse<>(new ReservationResponseDTO(reservation), "Successfully accepted reservation"))
@@ -191,7 +220,9 @@ public class ReservationController {
     @Path("/{accommodation_id}")
     @PermitAll
     public Response getReservationsForAccommodation(@PathParam("accommodation_id") long accommodationId) {
+        logger.info("Retrieving reservations for accommodation with id {}", accommodationId);
         List<Reservation> reservations = reservationService.findReservationsBasedOnAccommodation(accommodationId);
+        logger.info("Successfully retrieved reservations for accommodation with id {}", accommodationId);
         List<ReservationResponseDTO> reservationResponseDTOS = new ArrayList<>();
         for (Reservation reservation: reservations) {
             reservationResponseDTOS.add(new ReservationResponseDTO(reservation));
@@ -210,7 +241,9 @@ public class ReservationController {
     public Response areReservationsActive(@HeaderParam("Authorization") String authorizationHeader,
                                           @PathParam("email") String email) {
         boolean areReservationsActive = true;
+        logger.info("Retrieving active reservations");
         List<Reservation> reservations = reservationService.getActiveReservations(email);
+        logger.info("Successfully retrieved active reservations");
         if (reservations.size() == 0) {
             areReservationsActive = false;
         }
